3c17c3787a3137a86278a7abe682787e3fc8cd73
kchranger <kchranger@gmail.com>
2024-07-30 15:30:50 +0900

    refactor 2

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,87 +14,69 @@ class GildedRose {
     }
 
 
-    /**
-     * 1. 특별 물건 / 일반 물건 구분
-     * 2. sellIn 1 감소
-     * 3. Quality 1 감소
-     * */
     public void updateQuality() {
         for (Item item : items) {
+            int newQuality = 0;
             String itemName  = item.name;
-            if (itemName.contains(AGED_BRIE)) {
-                calAgedBrie(item);
-            } else if (itemName.contains(BACKSTAGE_PASSES)) {
-                calBackstagePass(item);
-            } else if (itemName.contains(SULFURAS)) {
-                calSulfuras(item);
-            } else if (itemName.contains(CONJURED)) {
-                calConjured(item);
-            } else {
-                calDefault(item);
-            }
-
+            newQuality = getNewQuality(item, itemName);
+            item.quality = newQuality;
             if (itemName.contains(SULFURAS)) continue;
             item.sellIn--;
         }
     }
 
-    /**
-     * 공통 :
-     * Q는 음수가 안된다.
-     * Q의 최대는 50이다.
-     * SellIn이 0이면, Q는 2배로 떨어진다.
-     */
-
-    /**
-     * Q +1
-     */
-    private void calAgedBrie(Item item) {
-        if (item.sellIn <= 0) {
-            item.quality = Math.min(item.quality + 2, 50);
-        } else if (item.sellIn > 0) {
-            item.quality = Math.min(item.quality + 1, 50);
+    private int getNewQuality(Item item, String itemName) {
+        int newQuality;
+        if (itemName.contains(AGED_BRIE)) {
+            newQuality = getAgedBrieQuality(item);
+        } else if (itemName.contains(BACKSTAGE_PASSES)) {
+            newQuality = getBackstagePassQuality(item);
+        } else if (itemName.contains(SULFURAS)) {
+            newQuality = getSulfurasQuality(item);
+        } else if (itemName.contains(CONJURED)) {
+            newQuality = getConjuredQuality(item);
+        } else {
+            newQuality = getDefaultQuality(item);
         }
+        return newQuality;
     }
 
-    /**
-     * Q + 1
-     * 10 -> +2
-     * 5 -> + 3
-     * 0 -> q = 0
-     */
-    private void calBackstagePass(Item item) {
+    private int getAgedBrieQuality(Item item) {
+        if (item.sellIn <= 0)
+            return Math.min(item.quality + 2, 50);
+
+        return Math.min(item.quality + 1, 50); // sellIn > 0
+    }
+
+    private int getBackstagePassQuality(Item item) {
         int sellIn = item.sellIn;
-        if (sellIn > 10) {
-            item.quality = Math.min(item.quality + 1, 50);
-        } else if (sellIn <= 10 && sellIn > 5) {
-            item.quality = Math.min(item.quality + 2, 50);
-        } else if (sellIn <= 5 && sellIn > 0) {
-            item.quality = Math.min(item.quality + 3, 50);
-        } else if (sellIn == 0) {
-            item.quality = 0;
-        }
+
+        if (sellIn > 10)
+            return Math.min(item.quality + 1, 50);
+
+        if (sellIn <= 10 && sellIn > 5)
+            return Math.min(item.quality + 2, 50);
+
+        if (sellIn <= 5 && sellIn > 0)
+            return Math.min(item.quality + 3, 50);
+
+        return 0; // sellIn <= 0
     }
 
-
-    private void calSulfuras(Item item) {
-
+    private int getSulfurasQuality(Item item) {
+        return item.quality;
     }
 
-    private void calConjured(Item item) {
+    private int getConjuredQuality(Item item) {
         int weight = 1;
-        if (item.sellIn == 0) {
-            weight *= 2;
-        }
-        item.quality = Math.max(item.quality - (2 * weight), 0);
+        if (item.sellIn == 0) weight = 2;
+        return Math.max(item.quality - (2 * weight), 0);
     }
 
-    private void calDefault(Item item) {
+    private int getDefaultQuality(Item item) {
         int weight = 1;
-        if (item.sellIn == 0) {
-            weight *= 2;
-        }
-        item.quality = Math.max(item.quality - (1 * weight), 0);
+        if (item.sellIn == 0) weight = 2;
+        return Math.max(item.quality - (1 * weight), 0);
     }
 
 }
